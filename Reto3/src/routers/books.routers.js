b474613c1311967d17f1d43d7e3729e13319843e
Cristinahb <achbocourt@gmail.com>
2023-09-18 13:00:33 +0200

    subir cambios

--- Reto3/src/routers/books.routers.js
+++ Reto3/src/routers/books.routers.js
@@ -2,9 +2,11 @@ const {Router} = require("express");
 const router = Router();
 const booksCtrl = require ("../controller/books.controller");
 
-router.get("/books", booksCtrl.getBook);
+router.get("/books",booksCtrl.getBooks)
+router.get("/books/:id", booksCtrl.getBook);
 router.post("/books", booksCtrl.postBook);
 router.put("/books", booksCtrl.putBook);
 router.delete("/books", booksCtrl.deleteBook);
+
 
 module.exports = router;
